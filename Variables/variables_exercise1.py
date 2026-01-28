de438796c7e255aaaff0f8c8c320aad195077874
sinzwjhb025 <sizwjhb025@student.wethinkcode.co.za>
2026-01-28 14:28:38 +0200

    created variables with a string, bolean, , int and floats to print them in one sentence..

---
+++ Variables/variables_exercise1.py
@@ -0,0 +1,15 @@
+# Create variables for your name, age, and height. Print them in one sentence.
+
+name = "Marshal"
+age = 25
+height = 1.5
+# print(name ,  age, height)
+print(f"My name is {name} and I am {age} old. I am {height} meters tall")
+
+# Store a boolean value that represents whether you are a student. Print it.
+is_student = True
+print(is_student)
+
+# Convert your age into a string and concatenate it with your name in a sentence.
+age = '26'
+print("My name is Marshal and I am" + " " + str(age) + " " + "years old.")

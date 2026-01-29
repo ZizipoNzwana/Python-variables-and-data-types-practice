d2e96b2b11370986df196f9475535558843aeee9
sinzwjhb025 <sizwjhb025@student.wethinkcode.co.za>
2026-01-29 11:44:11 +0200

    Created a user input calculator

---
+++ operators.py
@@ -0,0 +1,72 @@
+# Calculate your BMI using weight / (height ** 2) and print the result.
+weight = 75
+height = 1.5 
+age = 18
+
+bmi = weight / (height ** 2)
+print(bmi)
+
+# # Compare your age with 18 and print whether you are older or younger.
+
+if age >= 18:
+    print("you are older")
+else:
+    print("you are younger")
+
+# # Use logical operators to check if your age is between 18 and 30.
+if age >= 18 or age == 30:
+    print("You are between 18 and 30 years")
+else:
+    print("You are below 18 and 19 years")
+    
+# # Write a program that checks if your BMI is in a healthy range (18–25).
+if float(bmi) >= age:
+    print("Your BMI is in a healthy range")
+else:
+    print("Your BMI is not in a healthy range")
+    
+# # Ask the user for their age and print whether they are a minor, adult, or senior.
+# age = int(input("What is your age?: "))
+
+if age >= 18:
+    print("You are a senior")
+elif age == 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+
+# # Write a program that checks if a password is "python123" and prints “Access granted” or “Access denied”.
+
+password = "python123"
+
+if password >= "":
+    print("Access granted!")
+else:
+    ("Access denied!")
+    
+    
+# Ask the user to enter two numbers and an operator (+, -, *, /).
+# Use conditionals to perform the correct operation and print the result.
+# Example: If the user enters +, add the numbers.
+
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+operator = input("Enter an operator (+, -, *, /, %): ")
+
+if operator == "+":
+    print("Result: ", num1 + num2)
+elif operator == "-":
+    print("Result: ", num1 - num2)
+elif operator == "*":
+    print("Result: ", num1 * num2)
+elif operator == "%":
+    print("Result: ", num1 %num2)
+
+
+
+
+
+
+

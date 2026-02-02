67775d669f34afcf726ab1f47b553c5b9189c5ce
sinzwjhb025 <sizwjhb025@student.wethinkcode.co.za>
2026-02-02 16:01:40 +0200

    Started with while loops

--- Loops.py
+++ Loops.py
@@ -1,2 +1,54 @@
-for count in range(1, 6):
-    print(count)
+# Challenge 1: Print Numbers
+# Write a for loop that prints numbers from 1 to 10.
+# for num in range(1, 11):
+#     print(num)
+
+# Challenge 2: Multiplication Table
+# Ask the user for a number, then use a for loop to print its multiplication table up to 10.
+
+# numbers = int(input("Enter a number: "))
+# for num in range(10, 0, -1):
+#     num * numbers
+#     print(f"{numbers} x {num} = {num *numbers}")
+# # total = 1
+pass
+# Challenge 2: Sum of Numbers
+# Use a for loop to add up all the numbers from 1 to 100 and print the total.
+
+# numbers = [ 3, 4, 5, 6]
+# for num in numbers:
+#     total *= num
+# print(total)
+pass
+# Challenge 3: Count Vowels
+# Write a program that loops through the string "programming" and counts how many vowels (a, e, i, o, u) are in it.
+
+# sentence = "programming"
+# vowels = "aeiou"
+# count = 0
+
+# for vowel in sentence:
+#     sentence = sentence.lower()
+#     if vowel in vowels:
+#         count += 1
+# print(count)
+    
+# Challenge 5: Reverse a Word
+# Ask the user to enter a word. Use a for loop to print the word backwards.
+# Example: "hello" → "olleh"
+
+# sentence = input("Enter sentence: ")
+
+# for word in sentence:
+#     word = sentence[::-1].lower()
+# print(word)
+
+
+# WHILE LOOPS....
+# Challenge 1: Countdown
+# Write a program that starts at 10 and counts down to 1 using a while loop. After reaching 1, print "Blast off!".
+# count_down = 10
+
+# while count_down < 10:
+#     count_down -= 1
+# print(count_down)

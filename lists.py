07784a0df6871865921b298765f31cb6f8ee2bf1
Sibuyiselwe Zizipo Nzwana <sinzwjhb025@student.wethinkcode.co.za>
2026-02-03 23:03:12 +0200

    do dictionaries

---
+++ lists.py
@@ -0,0 +1,71 @@
+# fruits = ["apple", "banana", "cherry"]
+
+# for i in range(len(fruits)):
+#     # print("Index:", i, "Value:", fruits[i])
+#     print(fruits[i -1])
+
+# Challenge 1: Lists
+
+# Create a list with 5 different data types.
+# Print the second item.
+# Change the third item.
+# Add a new item at the end.
+
+# list1 = ["Zizipo", 12, True, 1.58]
+# # print(list1[1])
+# # list1[2] = "Sophie"
+# # print(list1)
+# # list1.append(10)
+# # print(list1)
+# # list1.reverse()
+# # print(list1)
+# # list1.pop(2)
+# # print(list1)
+pass
+
+# Challenge 1: Iteration with Indexes
+# Create a list of 5 numbers.
+# Print each number with its index.
+
+# numbers = [1, 2, 3, 4, 5]
+
+# for num in range(len(numbers)):
+#    print(f"index: " , num, "value: ", numbers[num])
+
+pass
+# Challenge 2: Iteration with Items
+# Make a list of 4 favorite foods.
+# Print each food directly (no indexes).
+
+# food = ["apple", "spaghettii", "banana", "soup"]
+# for i in food:
+#    print(i)
+
+pass
+
+# Challenge 3: Manipulation
+
+# Start with a list: ["dog", "cat", "bird"].
+# Change "cat" to "lion".
+# Add "fish" at the end.
+# Remove "dog".
+# Print the final list.
+
+# list1 = ["dog", "cat", "bird"]
+# list1[1] = "lion"
+# # print(list1)
+# list1.append("fish")
+# # print(list1)
+# list1.remove("dog")
+# print(list1)
+
+# Challenge 4: Mixed List
+# Create a list with numbers and text mixed together.
+# Loop through it and print whether each item is a number or text.
+
+items = [1, "apple", "sophie", 2]
+
+for i in items:
+    print(i)
+    if i >= int:
+        print(i)
